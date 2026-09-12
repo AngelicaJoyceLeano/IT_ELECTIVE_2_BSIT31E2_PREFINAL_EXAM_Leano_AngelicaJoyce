94c1d3e1c05740bbfb186927b2b9752b792a9af4
AngelicaJoyceLeano <ajlleano13@gmail.com>
2026-09-12 11:12:45 +0800

    Added MVC

---
+++ PREFINAL_EXAM_Leano_AngelicaJoyce/PREFINAL_EXAM_Leano_AngelicaJoyce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINAL_EXAM_Leano_AngelicaJoyce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
