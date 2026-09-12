316ee8072873bda8b5b835f5680ecbe4c46e071f
AngelicaJoyceLeano <ajlleano13@gmail.com>
2026-09-12 12:46:52 +0800

    Added Question 20

--- PREFINAL_EXAM_Leano_AngelicaJoyce/PREFINAL_EXAM_Leano_AngelicaJoyce/Controllers/ExamController1.cs
+++ PREFINAL_EXAM_Leano_AngelicaJoyce/PREFINAL_EXAM_Leano_AngelicaJoyce/Controllers/ExamController1.cs
@@ -257,6 +257,19 @@ namespace IT_ELECTIVE_2_PREFINAL_EXAM.Controllers
                     },
                     YourAnswer = "B"
                 },
+                 new Question
+                {
+                    Number = 20,
+                    Text = "A student already belongs to Section A for a particular subject. The application attempts to assign the same student to Section A again. What is the primary concern?",
+                    Choices = new[]
+                    {
+                        "A. Data integrity",
+                        "B. HTML formatting",
+                        "C. CSS inheritance",
+                        "D. Razor syntax"
+                    },
+                    YourAnswer = "A"
+                }
 
             };
 
